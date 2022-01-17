a8b2f5614b351bd3d3d8cbfe936fbe0c50a8a9c0
tohsaka888 <156132264@qq.com>
2022-01-17 17:23:18 +0800

    获取屏幕可用大小

---
+++ components/hook/useScreenSize.tsx
@@ -0,0 +1,24 @@
+import React, { useCallback, useEffect, useState } from "react";
+
+function useScreenSize() {
+  const [width, setWidth] = useState<number>(0);
+  const [height, setHeight] = useState<number>(0);
+
+  const resizeEvent = useCallback(() => {
+    setWidth(window.innerWidth);
+    setHeight(window.innerHeight);
+  }, []);
+
+  useEffect(() => {
+    console.log(1);
+    setWidth(window.innerWidth);
+    setHeight(window.innerHeight);
+    window.addEventListener("resize", resizeEvent);
+    return () => {
+      window.removeEventListener("resize", resizeEvent);
+    };
+  }, [resizeEvent]);
+  return { height: height, width: width };
+}
+
+export default useScreenSize;
