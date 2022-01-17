ffcc60fe60e5bc539fa98c570539385404f1dc7e
tohsaka888 <156132264@qq.com>
2022-01-17 17:25:48 +0800

    提供layout和chakra主题

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,17 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import "antd/dist/antd.css"; // or 'antd/dist/antd.less'
+import { ChakraProvider, theme } from "@chakra-ui/react";
+import Layout from "../components/common/Layout";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider theme={theme}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </ChakraProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
