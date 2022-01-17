679df7b0ec831c7b5beac6abe4089f0295e41d11
tohsaka888 <156132264@qq.com>
2022-01-17 17:24:18 +0800

    样式组件

---
+++ components/styles/search.styles.tsx
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+const Score = styled.div`
+  color: tomato;
+  font-size: 24px;
+  font-weight: bold;
+  margin-left: 8px;
+`;
+
+const Source = styled.div`
+  color: lightcoral;
+  font-size: 18px;
+  font-weight: bold;
+  margin-left: 8px;
+`
+
+export { Score, Source };
