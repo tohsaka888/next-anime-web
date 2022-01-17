80941582f9a56e26659e6c50fbccb970d56ee9dc
tohsaka888 <156132264@qq.com>
2022-01-17 17:24:38 +0800

    动画信息

---
+++ components/Update/AnimeInfo.tsx
@@ -0,0 +1,58 @@
+import { Box, Flex, Text } from "@chakra-ui/react";
+import { Typography } from "antd";
+import Image from "next/image";
+import React, { useState } from "react";
+import ScaleElement from "../springs/ScaleElement";
+import { UpdatesProps } from "./type";
+import styles from "../../styles/Home.module.css";
+import { useRouter } from "next/router";
+
+function AnimeInfo({ cover_url, title, update_time, update_to }: UpdatesProps) {
+  const [isHover, setIsHover] = useState<boolean>(false);
+  const router = useRouter();
+
+  return (
+    <Flex
+      flexWrap={["wrap", "nowrap"]}
+      position={"relative"}
+      onClick={() => {
+        router.push(`/search/${title}`);
+      }}
+    >
+      <ScaleElement
+        isHover={isHover}
+        onMouseOver={() => {
+          setIsHover(true);
+        }}
+        onMouseLeave={() => {
+          setIsHover(false);
+        }}
+      >
+        <Image
+          src={cover_url}
+          alt="title"
+          width={"200px"}
+          height={"300px"}
+          className={styles.image}
+          priority={true}
+        />
+        <Typography style={{ width: "200px", lineHeight: 1, marginTop: "8px" }}>
+          <Typography.Paragraph
+            ellipsis={{ rows: 2 }}
+            style={{ fontSize: "1rem", fontWeight: "bold" }}
+          >
+            {title}
+          </Typography.Paragraph>
+          <Typography.Paragraph ellipsis={{ rows: 1 }}>
+            更新时间: {update_time}
+          </Typography.Paragraph>
+          <Typography.Paragraph ellipsis={{ rows: 1 }}>
+            更新至: {update_to}
+          </Typography.Paragraph>
+        </Typography>
+      </ScaleElement>
+    </Flex>
+  );
+}
+
+export default AnimeInfo;
