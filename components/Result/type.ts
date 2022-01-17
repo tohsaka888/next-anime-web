dbf83d813b6c7a89a328917fa76fad7e41a78ebc
tohsaka888 <156132264@qq.com>
2022-01-17 17:23:31 +0800

    Result类型

---
+++ components/Result/type.ts
@@ -0,0 +1,11 @@
+type ResultProps = {
+  category: string;
+  cover_url: string;
+  description: string;
+  module: string;
+  score: number;
+  title: string;
+  url: string;
+};
+
+export type { ResultProps };
