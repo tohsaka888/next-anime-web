68df38cee8ecbc8ab58b7eda0f104eb1a4465a29
tohsaka888 <156132264@qq.com>
2022-01-17 17:25:07 +0800

    Update类型

---
+++ components/Update/type.ts
@@ -0,0 +1,15 @@
+type UpdatesProps = {
+  cover_url: string;
+  title: string;
+  update_time: string;
+  update_to: string;
+};
+
+type DailyAnimeProps = {
+  date: string;
+  day_of_week: string;
+  is_today: boolean;
+  updates: UpdatesProps[];
+};
+
+export type { UpdatesProps, DailyAnimeProps };
