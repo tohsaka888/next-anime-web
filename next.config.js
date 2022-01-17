85939d2116ef43be432815a182ccbf7859bf293e
tohsaka888 <156132264@qq.com>
2022-01-17 17:22:01 +0800

    增加图片域名

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ['i0.hdslb.com', 'img.kuaibozy.net', 'img9.doubanio.com', 'sc04.alicdn.com', 'pic.wujinimg.com', 'pic.wujinimg.com', 'ae02.alicdn.com', 'lf6-ttcdn-tos.pstatp.com']
+  }
 }
 
 module.exports = nextConfig
