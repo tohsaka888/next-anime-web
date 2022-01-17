7617e174f65efcd0691438a0d2ba15b33d548dfd
tohsaka888 <156132264@qq.com>
2022-01-17 17:23:01 +0800

    侧边栏

---
+++ components/common/Sider.tsx
@@ -0,0 +1,32 @@
+import { Box, useColorMode } from "@chakra-ui/react";
+import React, { useEffect, useRef } from "react";
+import { Divider, Menu } from "antd";
+
+function Sider() {
+  const { colorMode } = useColorMode();
+  const heightRef = useRef<string | number>("92vh");
+  useEffect(() => {
+    heightRef.current = screen.availHeight;
+  }, []);
+  return (
+    <Box
+      w={["100px", "100px", "250px"]}
+      // borderRight={"1px solid #cecece"}
+      h={heightRef.current}
+      position={"fixed"}
+    >
+      <Menu
+        defaultSelectedKeys={["update"]}
+        theme={colorMode}
+        style={{ height: "100%", background: "transparent" }}
+      >
+        <Menu.Item key={"update"}>最近更新</Menu.Item>
+        <Menu.Item key={"history"}>播放历史</Menu.Item>
+        <Menu.Item key={"collect"}>用户收藏</Menu.Item>
+      </Menu>
+      <Divider />
+    </Box>
+  );
+}
+
+export default Sider;
