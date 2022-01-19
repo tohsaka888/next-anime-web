c5cbe71ec5aacb87555728e622459d8823a115a7
tohsaka888 <156132264@qq.com>
2022-01-19 10:26:51 +0800

    布局修改

--- components/common/Layout.tsx
+++ components/common/Layout.tsx
@@ -1,22 +1,28 @@
 import { Box, Flex } from "@chakra-ui/react";
-import React, { ReactNode, useEffect, useRef } from "react";
+import { useRouter } from "next/router";
+import React, { ReactNode, useEffect, useRef, useState } from "react";
 import Header from "./Header";
 import Sider from "./Sider";
-
+import { Context } from "../Update/context";
+import { Spin } from "antd";
 type Props = {
   children: ReactNode;
 };
 
 function Layout({ children }: Props): JSX.Element {
+  const router = useRouter();
+  const [loading, setLoading] = useState<boolean>(false);
   return (
     <Box position={"relative"}>
       <Header />
-      <Flex overflow={"auto"}>
-        <Sider />
-        <Box ml={"250px"}>
-          {children}
-        </Box>
-      </Flex>
+      <Context.Provider value={{ loading, setLoading }}>
+        <Flex overflow={"auto"}>
+          {!router.pathname.includes("/anime") && <Sider />}
+          <Box ml={router.pathname.includes("/anime") ? "0px" : "250px"}>
+            <Spin spinning={loading}>{children}</Spin>
+          </Box>
+        </Flex>
+      </Context.Provider>
     </Box>
   );
 }
