fcc84dec5ed074fb7df51e7590594b65771f5af6
tohsaka888 <156132264@qq.com>
2022-01-21 13:23:47 +0800

    加入Menu显示图标

--- components/common/Header.tsx
+++ components/common/Header.tsx
@@ -1,19 +1,12 @@
-import {
-  Box,
-  Flex,
-  IconButton,
-  Input,
-  InputGroup,
-  InputRightElement,
-  useColorMode,
-} from "@chakra-ui/react";
-import React, { useCallback, useEffect, useRef, useState } from "react";
-import { MdWbSunny, MdDarkMode, MdSearch } from "react-icons/md";
+import { Box, Flex, Icon, useColorMode } from "@chakra-ui/react";
+import React from "react";
+import { MdWbSunny, MdDarkMode } from "react-icons/md";
 import { AiFillGithub } from "react-icons/ai";
 import Image from "next/image";
 import styles from "../../styles/Home.module.css";
 import { useRouter } from "next/router";
 import SearchArea from "./SearchArea";
+import { MdMenu } from "react-icons/md";
 
 function Header() {
   const { colorMode, toggleColorMode } = useColorMode();
@@ -30,14 +23,30 @@ function Header() {
       top={0}
     >
       <Flex flex={1}>
-        <Box
-          className={styles.logo}
-          cursor={"pointer"}
-          onClick={() => {
-            router.push("/");
-          }}
-        >
-          <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+        <Box className={styles.logo} cursor={"pointer"}>
+          <Flex align="center">
+            <Icon
+              as={MdMenu}
+              w="25px"
+              h={"25px"}
+              mr={"8px"}
+              _active={{
+                opacity: 0.5,
+                background: "#cecece",
+                padding: "10px",
+                borderRadius: "50%",
+              }}
+            />
+            <Image
+              src="/vercel.svg"
+              alt="Vercel Logo"
+              width={72}
+              height={16}
+              onClick={() => {
+                router.push("/");
+              }}
+            />
+          </Flex>
         </Box>
       </Flex>
       <Box w={["35vw", "40vw"]} display={["none", "none", "block"]} flex={2}>
