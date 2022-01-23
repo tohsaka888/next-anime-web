6f1a1ac7133274ee2967ad4c12fb5968ac205b88
tohsaka888 <156132264@qq.com>
2022-01-23 16:00:00 +0800

    是否显示侧边栏

--- components/common/Layout.tsx
+++ components/common/Layout.tsx
@@ -1,11 +1,13 @@
 import { Box, Flex, useBreakpoint, useColorMode } from "@chakra-ui/react";
 import { useRouter } from "next/router";
-import React, { ReactNode, useEffect, useRef, useState } from "react";
+import React, { ReactNode, useState } from "react";
 import Header from "./Header";
 import { Context } from "../Update/context";
 import { Spin } from "antd";
 import SearchArea from "./SearchArea";
 import dynamic from "next/dynamic";
+import { MenuShowContext } from "./Context";
+import ContentPop from "../springs/ContentPop";
 type Props = {
   children: ReactNode;
 };
@@ -16,20 +18,21 @@ function Layout({ children }: Props): JSX.Element {
   const router = useRouter();
   const [loading, setLoading] = useState<boolean>(false);
   const breakpoint = useBreakpoint();
-  const { colorMode } = useColorMode();
+  const [menuShow, setMenuShow] = useState<boolean>(true);
+
   return (
     <Box position={"relative"}>
       <Context.Provider value={{ loading, setLoading }}>
-        <Header />
-        {breakpoint === "base" && <SearchArea />}
-        <Flex overflow={"auto"} justify={["center", "flex-start"]}>
-          {!router.pathname.includes("/anime") && <Sider />}
-          <Box
-            ml={router.pathname.includes("/anime") ? "0px" : ["0px", "15vw"]}
-          >
-            <Spin spinning={loading}>{children}</Spin>
-          </Box>
-        </Flex>
+        <MenuShowContext.Provider value={{ menuShow, setMenuShow }}>
+          <Header />
+          {breakpoint === "base" && <SearchArea />}
+          <Flex overflow={"auto"} justify={["center", "flex-start"]}>
+            {!router.pathname.includes("/anime") && <Sider />}
+            <ContentPop>
+              <Spin spinning={loading}>{children}</Spin>
+            </ContentPop>
+          </Flex>
+        </MenuShowContext.Provider>
       </Context.Provider>
     </Box>
   );
