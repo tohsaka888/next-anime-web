65cb0476a500d6d0f4140528b13d7b1e82bfb4a0
tohsaka888 <156132264@qq.com>
2022-01-23 16:00:56 +0800

    实验性功能

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,13 +3,22 @@ import type { AppProps } from "next/app";
 import "antd/dist/antd.css"; // or 'antd/dist/antd.less'
 import { ChakraProvider, theme } from "@chakra-ui/react";
 import Layout from "../components/common/Layout";
+import { WebsocketContext } from "../components/WebsocketContext";
+import { animeUrl, wsAnimeUrl } from "../config/baseUrl";
+import { useEffect, useRef, useState } from "react";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const wsRef = useRef<WebSocket>();
+  useEffect(() => {
+    wsRef.current = new WebSocket(`${wsAnimeUrl}`);
+  }, []);
   return (
     <ChakraProvider theme={theme}>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
+      <WebsocketContext.Provider value={{ wsRef }}>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </WebsocketContext.Provider>
     </ChakraProvider>
   );
 }
