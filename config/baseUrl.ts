03a9b7820f16e8a6ac21cde0e9bc5c9184df350a
tohsaka888 <156132264@qq.com>
2022-01-23 16:01:20 +0800

    主路由更改

--- config/baseUrl.ts
+++ config/baseUrl.ts
@@ -1,2 +1,4 @@
-export const animeUrl = "https://tohsaka888.cf/anime-api";
+// export const animeUrl = "https://tohsaka888.cf/anime-api";
+export const animeUrl = "http://81.68.113.218:6001";
+
 export const wsAnimeUrl = "ws://81.68.113.218:6001"
